1ebe97c2c49d9d7924b7314f359ca28c15b4e93b
swapnil jain <swapniltech0390@gmail.com>
2025-05-20 21:47:25 +0530

    clean client config

--- apps/products/src/app/services/product.service.ts
+++ apps/products/src/app/services/product.service.ts
@@ -2,14 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ApiResponse, Product } from "@medium/libs";
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   http = inject(HttpClient);
+  baseURL = environment.apiUrl;
 
   getProducts(): Observable<ApiResponse<Product[]>> {
-    return this.http.get<ApiResponse<Product[]>>('/api/products');
+    return this.http.get<ApiResponse<Product[]>>(`${this.baseURL}api/products`);
   }
 }
